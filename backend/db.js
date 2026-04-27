5d90f426c648af4826a63c360ef504df5ca19300
Gustavo Lanjoni <gustavosilva94514@gmail.com>
2026-04-26 23:13:34 -0300

    corrige conexao do banco para producao

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  connectionString: "postgresql://postgres:Gustavo%402393..@db.mwrjldjiogtohsxexqzy.supabase.co:5432/postgres",
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
